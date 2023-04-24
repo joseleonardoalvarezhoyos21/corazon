cdaed034942e08fa2ef5057d7c4da6717675ccdf
unknown <josealvarez1654@outlook.com>
2023-04-23 21:39:26 -0500

    corazon terminado

---
+++ README.md
@@ -0,0 +1,4 @@
+# heart
+a project for my girlfriend
+
+https://youtu.be/t7RwLrV5hyU
---
+++ js/main.js
@@ -0,0 +1,8 @@
+const typed = new Typed('.card__parrafo', {
+    strings: ['Te amo mucho mi amor y quiero toda esta vida contigo.'],
+    typeSpeed:100,
+    backSpeed:60,
+    backDelay:1000,
+    showCursor:false,
+    loop: true
+} );
